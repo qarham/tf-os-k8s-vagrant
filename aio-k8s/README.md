3e8fb16028fd8562cccf7c86e83443b396c41e0b
Qarham <qarham@juniper.net>
2018-04-15 13:01:21 -0500

    Added AIO separate folders for k8s and os + k8s

---
+++ aio-k8s/README.md
@@ -0,0 +1,2 @@
+# tf-os-k8s-vagrant
+Tungsten Fabric AIO K8s Vagrant Deployer
--- scripts/aio-ansible-k8s.sh
+++ scripts/aio-ansible-k8s.sh
@@ -16,11 +16,9 @@ cp /home/vagrant/instances.yaml ${OC_PATH}/config/
 cd ${OC_PATH}
 
 sudo ansible-playbook -i inventory/ playbooks/configure_instances.yml
-sudo ansible-playbook -i inventory/ -e orchestrator=openstack playbooks/install_contrail.yml
+sudo ansible-playbook -e orchestrator=kubernetes -i inventory/ playbooks/install_contrail.yml
 
-
-# Install Weave Scope for Conatiners monitoring
-sudo curl -L git.io/scope -o /usr/bin/scope
-sudo chmod a+x /usr/bin/scope
-scope launch
+echo ****************** Monitoring Software Installation ********
+# Weavescope will create a separate Namespace called "weave" and use NodePort use "kubectl get svc -n weave" for NodePort number
+kubectl apply -f "https://cloud.weave.works/k8s/scope.yaml?k8s-service-type=NodePort&k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
