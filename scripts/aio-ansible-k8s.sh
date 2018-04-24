0a858f88913e099154918af50e13b97ba117dc3f
Qarham <qarham@juniper.net>
2018-04-23 22:09:37 -0500

    Added taint for master

--- scripts/aio-ansible-k8s.sh
+++ scripts/aio-ansible-k8s.sh
@@ -18,6 +18,9 @@ cd ${OC_PATH}
 sudo ansible-playbook -i inventory/ playbooks/configure_instances.yml
 sudo ansible-playbook -e orchestrator=kubernetes -i inventory/ playbooks/install_contrail.yml
 
+echo ******* Taint k8s Master for PODs creation ***************
+kubectl describe pods weave-scope-app-766488b5d-4blgp -n weave
+
 echo ****************** Monitoring Software Installation ********
 # Weavescope will create a separate Namespace called "weave" and use NodePort use "kubectl get svc -n weave" for NodePort number
 kubectl apply -f "https://cloud.weave.works/k8s/scope.yaml?k8s-service-type=NodePort&k8s-version=$(kubectl version | base64 | tr -d '\n')"
