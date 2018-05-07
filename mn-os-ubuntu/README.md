a015e8a34d39d06ad2b094c387a597c0b1f5cb77
Qarham <qarham@juniper.net>
2018-05-07 12:50:00 -0500

    Added MN-OS Ubuntu code

---
+++ mn-os-ubuntu/README.md
@@ -0,0 +1,1 @@
+### Work in progress to update Readme 
---
+++ scripts/mn-ansible-os-ubuntu.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -v 
+
+sudo apt-get install -y git wget python-pip python-dev build-essential
+sudo wget https://launchpad.net/~ansible/+archive/ubuntu/ansible-2.4/+files/ansible_2.4.2.0-1ppa~zesty_all.deb
+sudo dpkg -i ansible_2.4.2.0-1ppa~zesty_all.deb
+
+
+# Download Contrail-Ansible-Deployer code For GA R5.0 use 1st command
+sudo git clone -b R5.0 https://github.com/Juniper/contrail-ansible-deployer /opt/contrail-ansible-deployer
+# sudo git clone https://github.com/Juniper/contrail-ansible-deployer.git /opt/contrail-ansible-deployer
+
+export BASE_DIR=/opt
+export OC_PATH=${BASE_DIR}/contrail-ansible-deployer
+
+cd ${BASE_DIR}
+
+rm -rf ${OC_PATH}/config/instances.yaml
+cp /home/vagrant/instances.yaml ${OC_PATH}/config/
+
+cd ${OC_PATH}
+
+sudo ansible-playbook -i inventory/ playbooks/configure_instances.yml
+sudo ansible-playbook -i inventory/ -e orchestrator=openstack playbooks/install_contrail.yml
+
+# Install Weave Scope for Conatiners monitoring
+sudo curl -L git.io/scope -o /usr/bin/scope
+sudo chmod a+x /usr/bin/scope
+scope launch
