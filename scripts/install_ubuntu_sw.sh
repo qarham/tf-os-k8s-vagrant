bf0d32c4cf16358036d776e2f40854b2c56af952
Qarham <qarham@juniper.net>
2018-05-14 15:04:59 -0500

    Ubuntu Packages and Script update

--- scripts/install_ubuntu_sw.sh
+++ scripts/install_ubuntu_sw.sh
@@ -5,8 +5,10 @@ sudo apt-get install \
      python-pip \
      python-dev \
      build-essential \
-    createrepo \
-    rpm2cpio  -y
+     python-pip
+     apt-transport-https \
+     createrepo \
+     rpm2cpio  -y
 
 sudo wget https://launchpad.net/~ansible/+archive/ubuntu/ansible-2.4/+files/ansible_2.4.2.0-1ppa~zesty_all.deb
 
