91714f0552c7f3f76abc1fe3b505cf88522ded2b
Qarham <qarham@juniper.net>
2018-06-09 14:44:04 -0500

    Some updates and fixes

--- cfm-1vqfx-5srv/scripts/set-centos-gw.sh
+++ cfm-1vqfx-5srv/scripts/set-centos-gw.sh
@@ -4,4 +4,5 @@ sudo sed  -i '/ONBOOT/a DEFROUTE="no"' /etc/sysconfig/network-scripts/ifcfg-eth0
 sudo sed  -i '/NETMASK/a GATEWAY=10.87.65.126' /etc/sysconfig/network-scripts/ifcfg-eth1
 #sudo sed -i 's/DEFROUTE="yes"/DEFROUTE="no"/' /etc/sysconfig/network-scripts/ifcfg-enp0s3
 #sudo sed  -i '/NETMASK/a GATEWAY=10.13.82.1' /etc/sysconfig/network-scripts/ifcfg-enp0s8
+sudo route add -host 1.1.1.1/32 gw 172.16.1.1
 sudo systemctl restart network
